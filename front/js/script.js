e2a4d5b7b8f49ee376e99d0fb93cd9181217768f
Yannick Bley <bleyyannickpro@gmail.com>
2022-10-07 18:54:56 +0200

    refactoring of fetching data

--- front/js/script.js
+++ front/js/script.js
@@ -1,31 +1,34 @@
 // Fetching data from the Backend API
 
-let items = document.getElementById("items");
-
 // fetch all products
-const getAllProducts = async () => {
-  const products = await fetch(`http://localhost:3000/api/products`)
-    .then((response) => {
+const getProducts = async () => {
+  const fetchProducts = await fetch(`http://localhost:3000/api/products`).then(
+    (response) => {
       if (response.ok) {
         return response.json();
       }
-    })
-    .then((data) => {
-      data.map((product) => {
-        items.insertAdjacentHTML(
-          "afterbegin",
-          `
-        <a href="./product.html?id=${product._id}">
-            <article>
-            <img src="${product.imageUrl}" alt="${product.altTxt}">
-            <h3 class="productName">${product.name}</h3>
-            <p class="productDescription">${product.description}</p>
-            </article>
-        </a>
-        `
-        );
-      });
-    });
-  return products;
+    }
+  );
+  return fetchProducts;
 };
-getAllProducts();
+const displayProducts = (products) => {
+  const container = document.getElementById("items");
+  products.then((data) => {
+    data.map((product) => {
+      container.insertAdjacentHTML(
+        "afterbegin",
+        `
+                 <a href="./product.html?id=${product._id}">
+                     <article>
+                     <img src="${product.imageUrl}" alt="${product.altTxt}">
+                     <h3 class="productName">${product.name}</h3>
+                     <p class="productDescription">${product.description}</p>
+                     </article>
+                 </a>
+                 `
+      );
+    });
+  });
+};
+const products = getProducts();
+displayProducts(products);
