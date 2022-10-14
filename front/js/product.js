953cda279a32eb274331c52d25b9cc91aec358e3
Yannick Bley <bleyyannickpro@gmail.com>
2022-10-14 17:50:59 +0200

    add colors product in product file

--- front/js/product.js
+++ front/js/product.js
@@ -5,9 +5,8 @@ const productId = params.get("id");
 const img = document.querySelector(".item__img");
 const price = document.getElementById("price");
 const description = document.getElementById("description");
-const colors = document.getElementById("colors");
+const selectColor = document.getElementById("colors");
 
-console.log(img);
 // Fetching product from backend API
 const getProduct = async () => {
   const product = await fetch(
@@ -20,13 +19,20 @@ const getProduct = async () => {
   return product;
 };
 
-const displayProduct = (data) => {
-  data.then((product) => {
+const displayProduct = (dataPromise) => {
+  dataPromise.then((product) => {
     img.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
     price.innerHTML = `${product.price}`;
     description.innerHTML = `${product.description}`;
+    const colorsProduct = product.colors;
+    const html = colorsProduct.map((colorProduct) =>
+      colorProductTextHTML(colorProduct)
+    );
+    selectColor.innerHTML += html.join("");
   });
 };
 
+const colorProductTextHTML = (colorProduct) =>
+  `<option value="${colorProduct}">${colorProduct}</option>`;
 const product = getProduct();
 displayProduct(product);
